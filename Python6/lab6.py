199d4340c2e4a8d6615c603b61a5151dde496897
Ale <alejdelcomp@gmail.com>
2019-10-29 09:39:33 -0600

    Update lab6.py

--- Python6/lab6.py
+++ Python6/lab6.py
@@ -15,41 +15,73 @@ def ReglaCrammer(A,b):
     return sol
 
 
-A = np.array([[2, -1, 5],
-              [3, -2, 1],
-             [-2,  2, 3]])
+A = np.array([[1, 1, 0],
+              [1, 0, 1],
+             [0,  1, 0]])
 
 
-b = np.array([[2],
-             [-3],
-              [5]])
+b = np.array([[1,1,0,1],
+             [1,0,1,0],
+              [0,1,0,0],
+              [0,1,0,0]])
 
-#determinantes
-DETA = np.linalg.det(A)                             #con numpy
-print("Determinante con numpy: \n " + str(DETA))
-DETA2 = sp.Matrix(A).det()                          #con sympy
-print(" \n Determinante con sympy: \n " + str(DETA2))
+C = np.random.randint(10,10, size=(100, 100)
+D = np.random.randint(10,10, size=(100, 1) 
+                                          
+b1= np.array([[1],
+               [2],
+               [4]])
+
+b2= np.array([[1],
+               [8],
+               [4],
+               [5]])
+
+print("matriz A: \n" + str(A))
+print("matriz B: \n" + str(b))
+print("matriz C: \n" + str(b))
+print("matriz D: \n" + str(b))
+print('\n------------------------------------------------------')  
+# Calcule los determinantes e inversas
+print("EJERICIO 1:")
+print('------------------------------------------------------\n') 
+DETA = np.linalg.det(A)                             #det con numpy
+print("Determinante A con numpy: \n " + str(DETA))
+DETA2 = sp.Matrix(A).det()                          #det con sympy
+print(" \n Determinante A con sympy: \n " + str(DETA2))
+
+DETB = np.linalg.det(b)                             
+print("Determinante B con numpy: \n " + str(DETB))
+DETB2 = sp.Matrix(b).det()                          
+print(" \n Determinante B con sympy: \n " + str(DETB2))
 
 #calcular la inversa
 inva = np.linalg.inv(A)                     #con numpy
-print("inversa con numpy: \n " + str(inva))
-inva2 = sp.Matrix(A).inv()                  #ccon sympy
-print("inversa con sympy: \n " + str(inva2))
+print("inversa A con numpy: \n " + str(inva))
+inva2 = sp.Matrix(A).inv()                  #con sympy
+print("inversa A con sympy: \n " + str(inva2))
 
+invb = np.linalg.pinv(b)                     
+print("inversa B con numpy: \n " + str(invb))
+invb2 = sp.Matrix(b).pinv()                  
+print("inversa B con sympy: \n " + str(invb2))
 
+print('\n------------------------------------------------------')  
+print("EJERICIO 2:")
+print('------------------------------------------------------\n') 
 #solucion del sistema
-inicio=tm.time()
-print(ReglaCrammer(A, b.transpose()))
-print("cramer: "+str(tm.time()-inicio))
+print("A𝑥 = 𝑏1")
 
 inicio=tm.time()
-print(np.dot(inva, b))
+print(np.dot(inva, b1))
 print("numpy: "+str(tm.time()-inicio))
 
 inicio=tm.time()
-print(np.dot(inva2, b))
+print(np.dot(inva2, b1))
 print("sympy: "+str(tm.time()-inicio))
 
+inicio=tm.time()
+print(ReglaCrammer(A, b1.transpose()))
+print("cramer: "+str(tm.time()-inicio))
 
-
-
+print("C𝑥 = 𝑏2")
